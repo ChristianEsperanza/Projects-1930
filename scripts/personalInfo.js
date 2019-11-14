c0c4ef4a41b6e162a10b709e59815c019e223205
ChristianEsperanza <christianesperanzaa@gmail.com>
2019-11-14 13:32:57 -0800

    Connects userinfo to database, probably can be modified more

--- scripts/personalInfo.js
+++ scripts/personalInfo.js
@@ -1,25 +1,27 @@
-// // Get the modal
-// var modal = document.getElementById("myModal");
 
-// // Get the button that opens the modal
-// var btn = document.getElementById("myBtn");
+/**
+ * Listens for user to click submit button, then it takes the specified 
+ * element and updates the database.
+ */
+document.getElementById('submit').addEventListener("click", processForm);
 
-// // Get the <span> element that closes the modal
-// var span = document.getElementsByClassName("close")[0];
+        function processForm(e) {
+            e.preventDefault();
 
-// // When the user clicks the button, open the modal 
-// btn.onclick = function() {
-//   modal.style.display = "block";
-// }
+            console.log("submit button clicked!")
 
-// // When the user clicks on <span> (x), close the modal
-// span.onclick = function() {
-//   modal.style.display = "none";
-// }
+            var dbref = db.collection("user").doc("test");
+            var age = document.getElementById('age').value;
+            var gender = document.getElementById('gender').value;
+            var program = document.getElementById('program').value;
 
-// // When the user clicks anywhere outside of the modal, close it
-// window.onclick = function(event) {
-//   if (event.target == modal) {
-//     modal.style.display = "none";
-//   }
-// }
+            /* Printing to the log */
+            console.log(age + " " + gender + " " + program);
+
+            dbref.set({
+                age: age,
+                gender: gender,
+                program: program
+            });
+
+        }
