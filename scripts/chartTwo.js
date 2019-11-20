a83421af1b3f22a4ef73829196e2039c671ce355
Jeongho Han <jungho1109@gmail.com>
2019-11-19 21:06:33 -0800

    Record page

---
+++ scripts/chartTwo.js
@@ -0,0 +1,33 @@
+const progress = document.querySelector("#progress");
+
+var bar = new ProgressBar.SemiCircle(progress, {
+  strokeWidth: 6,
+  color: '##FFE977',
+  trailColor: '#eee',
+  trailWidth: 1,
+  easing: 'easeInOut',
+  duration: 1400,
+  svgStyle: null,
+  text: {
+    value: '',
+    alignToBottom: false
+  },
+  from: {color: '#FFE977'},
+  to: {color: '#ED6A5A'},
+
+  step: (state, bar) => {
+    bar.path.setAttribute('stroke', state.color);
+    var value = Math.round(bar.value() * 30);
+    if (value === 0) {
+      bar.setText('');
+    } else {
+      bar.setText(value);
+    }
+
+    bar.text.style.color = state.color;
+  }
+});
+bar.text.style.fontFamily = '"Raleway", Helvetica, sans-serif';
+bar.text.style.fontSize = '2rem';
+
+bar.animate(0.8); 
