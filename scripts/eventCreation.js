b547deda68e130fd43786c8548f5e06be77ef9e4
ChristianEsperanza <christianesperanzaa@gmail.com>
2019-11-21 12:04:03 -0800

    push

--- scripts/eventCreation.js
+++ scripts/eventCreation.js
@@ -11,20 +11,27 @@ function processForm(e){
   var date = document.getElementById('inputDate').value;
   var time = document.getElementById('inputTime').value;
   var ampm = document.getElementById('inputAMPM').value;
-  var muscles = document.getElementById('musclesInput').value;
+  // var muscles = document.getElementById('musclesInput').value;
   var numOfBuddies = document.getElementById('select_numOfBuddies').value;
   var message = document.getElementById('text_event').value;
+  var muscles = document.getElementById('musclesInput').value;
 
-  var event = db.collection("event");
+  mBiceps = getElementById("#biceps");
 
-  event.add({
-    title: title,
-    date: date,
-    time: time + " " + ampm,
-    skillLevel: "ask Carly",
-    number: numOfBuddies,
-    message: message
-  });
+  if (mBiceps.isChecked()) {
+    db.document("gym").collection("4gk10LflK4tT8yXnekNZ").document("targetMuscles").value(true);
+  }
+
+  // var event = db.collection("event");
+
+  // event.add({
+  //   title: title,
+  //   date: date,
+  //   time: time + " " + ampm,
+  //   skillLevel: "ask Carly",
+  //   number: numOfBuddies,
+  //   message: message
+  // });
   console.log(["event created", title, date, time + " " + ampm, numOfBuddies + " people can join the event", "More details: \n" + message]);
 
 
