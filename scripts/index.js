cd0894024d070e83284efca99c277dac56f87ba0
ChristianEsperanza <christianesperanzaa@gmail.com>
2019-11-21 09:30:03 -0800

    success

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,7 @@
-// User document > Test collection
-
+    //Directs to the user database 
 db.collection('user').doc('test').onSnapshot(function (snap){
     console.log("Current data is...", snap.data());
+        //Customizes user index page to display the user's name which
+        //Is stored in the database
     document.getElementById("username").innerHTML = snap.data().name;
 });
