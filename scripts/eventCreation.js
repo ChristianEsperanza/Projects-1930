54f0c69c4ca03e4efef96a28612653e769e933f9
Roelle Kim <roelle723@gmail.com>
2019-11-27 15:15:44 -0800

    merge documents

--- scripts/eventCreation.js
+++ scripts/eventCreation.js
@@ -6,13 +6,13 @@
 
 
 
+
 //var values = $('#').val() -------------- for multi-selections
 //JSON object before writing it on the data base and print it right away.
 
-document.getElementById('eventCreation').addEventListener(submit, processForm);
+document.getElementById('eventCreation').addEventListener("submit", processForm);
 
 function processForm(event){
-  event.preventDefault();
   console.log("Yes button clicked")
   
 }
@@ -20,10 +20,14 @@ function processForm(event){
 
 function hah(e){
 
-  e.preventDefault();
-  console.log("Submit button click!")
+// function processForm(e){
+
+
+//   e.preventDefault();
+//   console.log("Submit button click!")
 
  
+
   var title = document.getElementById("inputTitle").value;
   var date = document.getElementById('inputDate').value;
   var time = document.getElementById('inputTime').value;
@@ -62,6 +66,27 @@ function hah(e){
   });
   console.log(["event created", title, date, time + " " + ampm, numOfBuddies + " people can join the event", "More details: \n" + message]);
 
+//   var title = document.getElementById("inputTitle").value;
+//   var date = document.getElementById('inputDate').value;
+//   var time = document.getElementById('inputTime').value;
+//   var ampm = document.getElementById('inputAMPM').value;
+//   var muscles = document.getElementById('musclesInput').value;
+//   var numOfBuddies = document.getElementById('select_numOfBuddies').value;
+//   var message = document.getElementById('text_event').value;
+
+//   var event = db.collection("event");
+
+//   event.add({
+//     title: title,
+//     date: date,
+//     time: time + " " + ampm,
+//     skillLevel: "ask Carly",
+//     number: numOfBuddies,
+//     message: message
+//   });
+//   console.log(["event created", title, date, time + " " + ampm, numOfBuddies + " people can join the event", "More details: \n" + message]);
+
+
 
   var event = db.collection("event");
 
@@ -76,4 +101,4 @@ function hah(e){
 
   console.log(["event created", title, date, time + " " + ampm, numOfBuddies + " people can join the event", "More details: \n" + message]);
 
-}
+// }
