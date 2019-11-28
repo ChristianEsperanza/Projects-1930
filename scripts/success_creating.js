dbc80cb30d5d1d7c993d054495eb0c6498f558e3
ChristianEsperanza <christianesperanzaa@gmail.com>
2019-11-27 18:05:05 -0800

    dont touch that js file

--- scripts/success_creating.js
+++ scripts/success_creating.js
@@ -1,10 +1,19 @@
-function getEvent(event) {
-    var db = db.collection(event).get().then(function(querySnapshot) {
-        querySnapshot.forEach(function (doc) {
-            var span = document.createElement("span");
-            document.getElementById("eventTitle").appendChild(span);
-            var node = document.createTextNode(doc.data().title);
-            span.appendChild(node);
-        });
-    });
-}
+var db = firebase.database();
+
+db.collection("event").onSnapshot(function(snapshot) {
+    snapshot.docChanges().forEach(function(change) {
+        if (change.type == "added") {
+            console.log("New Event: ", change.doc.data());
+        }
+    })
+});
+// function getEvent(event) {
+//     var db = db.collection(event).get().then(function(querySnapshot) {
+//         querySnapshot.forEach(function (doc) {
+//             var span = document.createElement("span");
+//             document.getElementById("eventTitle").appendChild(span);
+//             var node = document.createTextNode(doc.data().title);
+//             span.appendChild(node);
+//         });
+//     });
+// }
